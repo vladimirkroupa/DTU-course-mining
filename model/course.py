ab68e237c1243ea23352ca29f6f467f8ab2bae46
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-10-04 15:32:55 +0200

    Removed dependencies on Scrapy Item from Course.

--- model/course.py
+++ model/course.py
@@ -1,13 +1,13 @@
 class Course:
     
-    def __init__(self, course_item):
-        self.code = course_item['code']
-        self.language = course_item['language']
-        self.title_en = course_item['title_en']
-        self.title_da = course_item['title_da']
-        self.evaluation_type = course_item['evaluation_type']
-        self.ects_credits = course_item['ects_credits']
-        self.course_type = course_item['course_type']
+    def __init__(self, code, language, title_en, title_da, eval_type, ects, course_type):
+        self.code = code
+        self.language = language
+        self.title_en = title_en
+        self.title_da = title_da
+        self.evaluation_type = eval_type
+        self.ects_credits = ects
+        self.course_type = course_type
 
     def is_7_grade_eval(self):
         pass
