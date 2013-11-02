b949594d2befff567ad1976d95cdfeab1ce90670
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-11-02 18:54:21 +0100

    Fixed default argument.

--- util/scrapy_utils.py
+++ util/scrapy_utils.py
@@ -5,7 +5,7 @@ class PageStructureException(Exception):
 def select_single(selector, xpath):
     return select(selector, xpath, expected = 1)[0]
 
-def select(selector, xpath, expected = None):
+def select(selector, xpath, expected):
     result = selector.select(xpath)
     if expected is not None and len(result) != expected:
         msg = "Expected {0} result(s), got {1}. XPath expression: {2} Selector content: {3}".format(expected, len(result), xpath, selector.extract().encode('utf-8'))
