93b7e0ee6854a97ff95bf73e33a004dafcd538dc
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-12-03 15:57:05 +0100

    Updated repository to accept Items.

--- storage/course_repository.py
+++ storage/course_repository.py
@@ -12,7 +12,7 @@ class CourseRepository(object):
 
         self.departments = Table('departments', metadata,
                             Column('id', Integer, primary_key=True),
-                            Column('code', String),
+                            Column('dep_code', String),
                             Column('name_en', String))
 
         self.courses = Table('courses', metadata,
@@ -31,19 +31,19 @@ class CourseRepository(object):
     def __del__(self):
         self.db.dispose()
 
-    def store_course(self, course):
-        dep_id = self.find_department_by_code(course.department.code)
+    def store_course_item(self, course):
+        dep_id = self.find_department_by_code(course['department']['code'])
         if not dep_id:
-            dep_id = self.store_department(course.department)
+            dep_id = self.store_department_item(course['department'])
 
         ins = self.courses.insert().values(
-            code = course.code,
-            language = course.language,
-            title_en = course.title_en,
-            title_da = course.title_da,
-            evaluation_type = course.evaluation_type,
-            ects_credits = course.ects_credits,
-            course_type = course.course_type,
+            code = course['code'],
+            language = course['language'],
+            title_en = course['title_en'],
+            title_da = course['title_da'],
+            evaluation_type = course['evaluation_type'],
+            ects_credits = course['ects_credits'],
+            course_type = course['course_type'],
             department_id = dep_id
         )
 
@@ -60,14 +60,14 @@ class CourseRepository(object):
         else:
             return courses[0]
 
-    def store_department(self, department):
-        ins = self.departments.insert().values(code=department.code, name_en=department.name_en)
+    def store_department_item(self, department):
+        ins = self.departments.insert().values(dep_code=department['code'], name_en=department['title_en'])
         res = self.db.execute(ins)
         return res.inserted_primary_key[0]
 
     def find_department_by_code(self, code):
         code = unicode(code)
-        sel = select([self.departments]).where(self.departments.c.code == code)
+        sel = select([self.departments]).where(self.departments.c.dep_code == code)
         result = self.db.execute(sel)
         departments = [self._map_department(row) for row in result]
         if len(departments) == 0:
@@ -90,7 +90,7 @@ class CourseRepository(object):
 
     def _map_department(self, row_proxy):
         return Department(
-            code = row_proxy['code'],
+            code = row_proxy['dep_code'],
             title_en = row_proxy['name_en'],
             title_da = None
         )
--- storage/tests/test_course_repository.py
+++ storage/tests/test_course_repository.py
@@ -1,4 +1,5 @@
 import unittest
+from scraper.items import CourseItem, DepartmentItem
 from model.department import Department
 from model.course import Course
 from storage.course_repository import CourseRepository
@@ -9,19 +10,36 @@ class CourseRepositoryTest(unittest.TestCase):
         self.test_object = CourseRepository()
 
     def test_store_find_department(self):
-        department_1 = Department('24', 'National Veterinary Institute', None)
-        department_2 = Department('23', 'National Food Institute', None)
-        self.test_object.store_department(department_1)
-        self.test_object.store_department(department_2)
+        dep_1 = DepartmentItem(code='24', title_en='National Veterinary Institute', title_da=None)
+        dep_2 = DepartmentItem(code='23', title_en='National Food Institute', title_da=None)
+        self.test_object.store_department_item(dep_1)
+        self.test_object.store_department_item(dep_2)
 
+        expected_1 = Department('24', 'National Veterinary Institute', None)
+        expected_2 = Department('23', 'National Food Institute', None)
         actual_1 = self.test_object.find_department_by_code('24')
         actual_2 = self.test_object.find_department_by_code('23')
-        self.assertEqual(department_1, actual_1)
-        self.assertEqual(department_2, actual_2)
+        self.assertEqual(expected_1, actual_1)
+        self.assertEqual(expected_2, actual_2)
         self.assertIsNone(self.test_object.find_department_by_code('25'))
 
     def test_store_find_course(self):
-        course = Course(
+        course = CourseItem(
+            code = '11120',
+            language = 'English',
+            title_en = 'Daylight in buildings',
+            title_da = u'Dagslys i bygninger',
+            evaluation_type = '7 step scale',
+            ects_credits = 5.0,
+            course_type = 'MSc',
+            department = DepartmentItem(
+                code = '11',
+                title_en = 'Department of Civil Engineering',
+                title_da = None
+            )
+        )
+
+        expected = Course(
             code = '11120',
             language = 'English',
             title_en = 'Daylight in buildings',
@@ -36,7 +54,9 @@ class CourseRepositoryTest(unittest.TestCase):
             )
         )
 
-        self.test_object.store_course(course)
+        self.test_object.store_course_item(course)
+        actual = self.test_object.find_course_by_code('11120')
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
