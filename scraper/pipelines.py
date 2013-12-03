611f964506cdc1956408ba224866019940a3bd36
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-12-03 18:31:27 +0100

    Added pipeline to store courses in DB.

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -1,8 +1,20 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import log
+from scraper.items import CourseItem
+from storage.course_repository import CourseRepository
 
-class ScraperPipeline(object):
+class RepositoryPipeline(object):
+
+    def __init__(self):
+        self.repository = CourseRepository()
+
     def process_item(self, item, spider):
-        return item
+        if isinstance(item, CourseItem):
+            self.repository.store_course_item(item)
+        else:
+            raise ValueError("Unexpected item type " + type(item))
+
+    def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        pass
