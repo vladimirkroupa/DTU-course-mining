59e588ab2bdbfcc386afed4d2fd3fb3674fc3080
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-12-06 23:38:42 +0100

    Added cmdline interface.

---
+++ run.py
@@ -0,0 +1,34 @@
+import argparse
+from storage.course_repository import CourseRepository
+
+repo = CourseRepository('sqlite:///courses.db')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("course_code", help="course code")
+    parser.add_argument("-t", "--transitive", help="prints transitive graph", action="store_true")
+    args = parser.parse_args()
+    code = args.course_code
+    transitive = args.transitive
+
+    if not transitive:
+        plot_course_graph(code)
+    else:
+        plot_course_trans_graph(code)
+
+def plot_course_graph(code):
+    course = repo.find_course_by_code(code)
+    if not course:
+        raise ValueError("Course " + code + " not found in database.")
+    graph = course.prereq_graph()
+    graph.write(code+'.png', format='png')
+
+def plot_course_trans_graph(code):
+    course = repo.find_course_by_code(code)
+    if not course:
+        raise ValueError("Course " + code + " not found in database.")
+    graph = course.transitive_prereq_graph(repo, 0)
+    graph.write(code+'_trans.png', format='png')
+
+if  __name__ =='__main__':
+    main()
--- x.py
+++
@@ -1,9 +0,0 @@
-import storage.course_repository
-
-repo = storage.course_repository.CourseRepository('sqlite:///courses.db')
-
-course = repo.find_course_by_code("27420")
-
-graph = course.transitive_prereq_graph(repo, 0)
-
-graph.write('27420.png', format='png')
