f4a07d0ade87b71e1cae6a4ecb24622af7688072
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-12-11 15:28:34 +0100

    Fixed CLI.

--- run.py
+++ run.py
@@ -7,15 +7,15 @@ repo = CourseRepository('sqlite:///courses.db')
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("course_code", help="course code")
-    parser.add_argument("-t", "--transitive", help="prints transitive graph", action="store_true")
+    parser.add_argument("-t", dest="depth", help="prints transitive graph with depth T")
     args = parser.parse_args()
     code = args.course_code
-    transitive = args.transitive
+    depth = args.depth
 
-    if not transitive:
+    if not depth:
         plot_course_graph(code)
     else:
-        plot_course_trans_graph(code)
+        plot_course_trans_graph(code, int(depth))
 
 def plot_course_graph(code):
     course = repo.find_course_by_code(code)
@@ -24,11 +24,11 @@ def plot_course_graph(code):
     graph = course.prereq_graph()
     graph.write(code+'.png', format='png')
 
-def plot_course_trans_graph(code):
+def plot_course_trans_graph(code, depth):
     course = repo.find_course_by_code(code)
     if not course:
         sys.exit("Course " + code + " not found in database.")
-    graph = course.transitive_prereq_graph(repo, 0)
+    graph = course.transitive_prereq_graph(repo, depth)
     graph.write(code+'_trans.png', format='png')
 
 if  __name__ =='__main__':
